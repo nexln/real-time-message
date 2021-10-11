7c367713e418b036f09110af4cafaede87c3f023
nexln <nexliln@gmail.com>
2021-10-11 20:30:37 +0300

    try'a EventSourcing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import {LongPulling} from "./LongPulling";
+import {EventSourcing} from "./EventSourcing";
 
 function App() {
   return (
     <div className="App">
-      <LongPulling/>
+      <EventSourcing/>
     </div>
   );
 }
